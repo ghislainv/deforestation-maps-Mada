027ccf7dafc7980372b1044b7c22974f63f5693a
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-06-06 17:35:42 +0200

    Corrections by Tom

--- deforestmap.R
+++ deforestmap.R
@@ -924,10 +924,9 @@ options(knitr.kable.NA = "-")
 ## Knit and translate to html and pdf
 dir.create("report")
 ## Main document
-render("deforestmap.Rmd",output_dir="report") # html output
+render("deforestmap.Rmd",output_format=c("pdf_document","html_document"),output_dir="report") # html output
 ## Cover letter
 render("coverletter.md",output_format=c("pdf_document"),output_dir="report") # pdf output
-#render("deforestmap.Rmd",output_format=c("pdf_document","word_document"),output_dir="report")
 
 ##===========================================================================
 ## End of script
